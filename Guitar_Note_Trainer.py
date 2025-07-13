bfd18c0adf53218957951c3dcc8573f1e1102ba0
safat mobid <safatmobid@gmail.com>
2025-07-13 18:25:43 -0400

    program can locate audio files now, removed redundant file path finding code

--- Guitar_Note_Trainer.py
+++ Guitar_Note_Trainer.py
@@ -7,14 +7,21 @@ from playsound3 import playsound
 # #file_path = os.path.abspath("Audio_Clips/B_Note.mp3")
 # os.startfile(a_note)
 
-notes = os.path.join(os.path.dirname(__file__), "Audio_Clips")
+# notes = os.path.join(os.path.dirname(__file__), "Audio_Clips")
 
-a_note = os.path.join(notes, "A_Note.mp3")
-b_note = os.path.join(notes, "B_Note.mp3")
-d_note = os.path.join(notes, "D_Note.mp3")
-e_note = os.path.join(notes, "Upper_E_Note.mp3")
-g_note = os.path.join(notes, "G_Note.mp3")
-low_e_note = os.path.join(notes, "Lower_e_Note.mp3")
+# a_note = os.path.join(notes, "A_Note.mp3")
+# b_note = os.path.join(notes, "B_Note.mp3")
+# d_note = os.path.join(notes, "D_Note.mp3")
+# e_note = os.path.join(notes, "Upper_E_Note.mp3")
+# g_note = os.path.join(notes, "G_Note.mp3")
+# low_e_note = os.path.join(notes, "Lower_e_Note.mp3")
+
+a_note = "Audio_Clips/A_Note.mp3"
+b_note = "Audio_Clips/B_Note.mp3"
+d_note = "Audio_Clips/D_Note.mp3"
+e_note = "Audio_Clips/Upper_E_Note.mp3"
+g_note = "Audio_Clips/G_Note.mp3"
+low_e_note = "Audio_Clips/Lower_e_Note.mp3"
 
 
 playsound(a_note)
