e38f4ee7189848af104dfa30d79cacdd5ef3a135
safat mobid <safatmobid@gmail.com>
2025-07-13 18:42:21 -0400

    Added random note selection and tested audio playback

--- Guitar_Note_Trainer.py
+++ Guitar_Note_Trainer.py
@@ -1,5 +1,6 @@
-import os
+# import os
 from playsound3 import playsound
+import random
 
 # #print(os.getcwd()) #shows my directory
 
@@ -23,10 +24,37 @@ e_note = "Audio_Clips/Upper_E_Note.mp3"
 g_note = "Audio_Clips/G_Note.mp3"
 low_e_note = "Audio_Clips/Lower_e_Note.mp3"
 
+# playsound(a_note)
+# playsound(b_note)
+# playsound(d_note)
+# playsound(e_note)
+# playsound(g_note)
+# playsound(low_e_note)
 
-playsound(a_note)
-playsound(b_note)
-playsound(d_note)
-playsound(e_note)
-playsound(g_note)
-playsound(low_e_note)
+ran_num_gen = random.randint(1,6)
+
+if ran_num_gen == 1:
+    note_name = "A"
+    note_audio = a_note
+
+elif ran_num_gen == 2:
+    note_name = "B"
+    note_audio = b_note
+
+elif ran_num_gen == 3:
+    note_name = "D"
+    note_audio = d_note
+
+elif ran_num_gen == 4:
+    note_name = "E"
+    note_audio = e_note
+
+elif ran_num_gen == 5:
+    note_name = "G"
+    note_audio = g_note
+
+elif ran_num_gen == 6:
+    note_name = "Low_E"
+    note_audio = low_e_note
+
+playsound(note_audio)
