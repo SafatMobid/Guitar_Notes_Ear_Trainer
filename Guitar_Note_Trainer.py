7f436244fdc5fefb2d19c102732f545d6e3c2e26
safat mobid <safatmobid@gmail.com>
2025-07-13 18:14:15 -0400

    Added the rest of the note audio files

--- Guitar_Note_Trainer.py
+++ Guitar_Note_Trainer.py
@@ -7,9 +7,14 @@ from playsound3 import playsound
 # #file_path = os.path.abspath("Audio_Clips/B_Note.mp3")
 # os.startfile(a_note)
 
-audio_folder = os.path.join(os.path.dirname(__file__), "Audio_Clips")
+notes = os.path.join(os.path.dirname(__file__), "Audio_Clips")
 
-a_note = os.path.join(audio_folder, "A_Note.mp3")
+a_note = os.path.join(notes, "A_Note.mp3")
+b_note = os.path.join(notes, "B_Note.mp3")
+d_note = os.path.join(notes, "D_Note.mp3")
+e_note = os.path.join(notes, "Upper_E_Note.mp3")
+g_note = os.path.join(notes, "G_Note.mp3")
+low_e_note = os.path.join(notes, "Lower_e_Note.mp3")
 
 
 playsound(a_note)
